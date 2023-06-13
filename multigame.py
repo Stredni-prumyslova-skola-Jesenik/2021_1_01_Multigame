9c99e14ee564f0371a61f1388fdbc264cdb752b2
Daniel Bakes <129839836+PabloEmilioEscobarGaviria128482@users.noreply.github.com>
2023-06-13 11:18:33 +0200

    Uprava vzhledu menu
    Uprava vzhledu menu. Není dokončeno.

--- multigame.py
+++ multigame.py
@@ -2,15 +2,31 @@ import tkinter as tk
 import subprocess
 
 root = tk.Tk()
-
 root.geometry('500x300')
 root.title('Multigame')
+
+menu = tk.Menu(root)
+
+def ovladani_pong():
+    subprocess.Popen(['python', 'ovladani_pong.txt'])
+def ovladani_tetris():
+    subprocess.Popen(['python', 'ovladani_tetris.txt'])
 def open_tetris():
     subprocess.Popen(['python', 'tetris.py'])
 def open_pong():
     subprocess.Popen(['python', 'pong.py'])
 label = tk.Label(root, text='Choose your game!', font=('Arial', 18))
 label.pack(padx=20, pady=20)
+
+file_menu = tk.Menu(menu, tearoff=False)
+file_menu.add_command(label='Exit', command=quit)
+menu.add_cascade(label='File', menu=file_menu)
+
+ovladani_hry = tk.Menu(menu, tearoff=False)
+ovladani_hry.add_command(label='Pong Ovládání', command=ovladani_pong)
+ovladani_hry.add_command(label='Tetris Ovládání', command=ovladani_tetris)
+menu.add_cascade(label='Ovládání Hry', menu=ovladani_hry)
+
 
 button = tk.Button(root,
                 text='Pong',
@@ -23,5 +39,12 @@ button2 = tk.Button(root,
                     command=open_tetris,
                     font=('Arial', 18))
 button2.pack(padx=10, pady=10)
-    
+
+button3 = tk.Button(root,
+                text='Ovládání',
+                command=open_pong,
+                font=('Arial', 18))
+button.pack(padx=10, pady=10)
+
+root.configure(menu=menu)
 root.mainloop()
---
+++ ovladani_tetris.py
@@ -0,0 +1,1 @@
+#Tetris -- Díly Tetrisu mají tvar jednoho ze čtyř písmen–I, J,L,O,S,T,Z. Náhodně padající díly je potřeba skládat do jedné řady, přičemž s dílem je možné různě otáčet. Když vytvoříte úplný řádek bez děr, řada zmizí a ostatní čtverečky umístěné nad ní, se posunou dolů. V průběhu hry kostičky padají stále rychleji a rychleji.
